eea463400b1b4d51cb714113bdb25b744f8fc5ee
herry88 <herry081288@gmail.com>
2021-11-01 14:40:00 +0700

    latihan debug

--- latihan3.py
+++ latihan3.py
@@ -1,6 +1,3 @@
-from typing import Counter
-
-
 awal = int(input('Set Nilai Awal = '))
 akhir = int(input('Set Nilai Akhir = '))
 
@@ -8,3 +5,11 @@ count = 0
 summ = 0 
 print('Bilangan antara %d dan %d'%(awal, akhir))
 
+#before
+# for i in range(awal, akhir+1){
+#     print (i, end=' ')
+#     count = count+1
+#     summ = summ+i
+# }
+print('Bilangan diatas ada %d bilangan' %count)
+print('Jumlah semua Bilangan adalah  %d' %summ)
